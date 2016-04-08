f2025402188886ff527e33ef6eeb3e6ba02a1dc7
Nathaniel Edwards <nedwards@gmail.com>
2016-04-07 20:06:49 -0700

    Improve styling of welcome and loading scenes

--- penguin/app/components/loading.js
+++ penguin/app/components/loading.js
@@ -1,1 +1,39 @@
-loading.js
+var React = require('react-native');
+
+var {
+  ActivityIndicatorIOS,
+  View,
+  Text,
+  Component,
+  StyleSheet
+} = React;
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    flexDirection: 'column',
+    backgroundColor: '#48BBEC'
+  },
+  loadingText: {
+    marginTop: 100,
+    marginBottom: 150,
+    fontSize: 40
+  }
+});
+
+var Loading = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.loadingText}>Finding you your best match...</Text>
+      <ActivityIndicatorIOS
+      animating={true}
+      color="black"
+      size="large"
+      style={{transform: [{scale: 3}]}}>
+      </ActivityIndicatorIOS>
+    </View>
+  )
+}
+
+module.exports = Loading;
