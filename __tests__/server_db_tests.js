f6f01f841e18742164d92921ada4dea410281b0d
Rahim <rahim.dharssi@gmail.com>
2016-04-07 21:52:16 -0700

    Start writing tests for mongodb

--- __tests__/server_db_tests.js
+++ __tests__/server_db_tests.js
@@ -1,12 +1,13 @@
 // Require depedencies needed for testing
 jest.unmock('jquery');
 var $ = require('jquery');
+
 // Import server dependances jest.unmock('../server.js');
 // var server = require('../server');
 // Import database dependencies jest.unmock('../db/config.js');
 // var db = require('../db/config');
 
-console.log(jasmine.DEFAULT_TIMEOUT_INTERVAL);
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 describe('Basic server functionality', () => {
   it('Should accept and respond to GET requests at the /match endpoint', (done) => {
@@ -30,5 +31,14 @@ describe('Basic server functionality', () => {
       expect(status).toEqual('error');
       done();
     });
+  });
+});
+
+describe('Basic database functionality', () => {
+  it('Should be able to add documents to the users table', (done) => {
+
+  });
+  it('Should be able to delete documents from the users table', (done) => {
+
   });
 });
