39e668bd47d8766b03e59ae7d84385410d997b41
michelleheh <xiaoyu.he22@gmail.com>
2016-04-07 18:09:37 -0700

    pseudo coded signin and get home request handler

--- server/config/requestHandler.js
+++ server/config/requestHandler.js
@@ -1,13 +1,27 @@
 module.exports = {
-  getHome: function (req, res) { 
+  getHome: function(req, res) {
+    // TODO:
+    // if user is aleady connected with FB
+      // redirect to /welcome
+    // else
     res.redirect('/signin'); 
   },
 
-  getSignin: function (req, res) { 
+  getSignin: function(req, res) { 
+    // TODO:
+    // if signin successful: check user in database
+      // redirect to welcome page
+    // else if user doesn't exist
+      // sign user up: store user info in database
     res.send('signing in...'); 
   },
 
-  getMatch: function (req, res) { 
-    res.send('Matching...'); 
-  } 
+  getMatch: function(req, res) { 
+    // TODO: implement web sockets    
+    res.send('Matching...');
+  },
+
+  getWelcome: function(req, res) {
+
+  }
 };
