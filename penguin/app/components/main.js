67f32b1ef06b22df0c0418bdc4037b1fb3e66a48
Nathaniel Edwards <nedwards@gmail.com>
2016-04-07 17:38:03 -0700

    Set up navigatorIOS and signin route / button

--- penguin/app/components/main.js
+++ penguin/app/components/main.js
@@ -1,1 +1,77 @@
-main.js
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  StyleSheet,
+  Component,
+  TouchableHighlight
+} = React;
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
+
+class Main extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      // ?
+    }
+  }
+
+  handleSubmit(){
+    console.log('insert OAuth integration here');
+    // this.props.navigator.push({
+    //   title: "Welcome",
+    //   component: Welcome,
+      // passProps: {userInfo: res} 
+      // should pass user ID, other details as received from OAuth
+    // });
+  }
+
+  render(){
+    return (
+      <View style={styles.mainContainer}>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="white">
+          <Text style={styles.buttonText}>Sign in with Facebook</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+};
+
+module.exports = Main;
--- penguin/index.ios.js
+++ penguin/index.ios.js
@@ -8,24 +8,24 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View
 } from 'react-native';
+
+// import * as Main from './app/components/main';
+
+var Main = require('./app/components/main');
 
 class penguin extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS 
+        style={styles.container}
+        initialRoute={{
+          title: 'Sign in with Facebook',
+          component: Main
+        }}
+      />
     );
   }
 }
@@ -33,8 +33,8 @@ class penguin extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
