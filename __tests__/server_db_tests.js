7455c261441cd7301f873d29cfbb3fbc960ede12
Rahim <rahim.dharssi@gmail.com>
2016-04-10 13:32:17 -0700

    Add dependencies to server/db test spec

--- __tests__/server_db_tests.js
+++ __tests__/server_db_tests.js
@@ -8,6 +8,16 @@ var Promise = require('bluebird');
 jest.unmock('mongoose');
 var mongoose = require('mongoose');
 
+jest.unmock('../server/server.js');
+jest.unmock('express');
+jest.unmock('../server/config/routes.js');
+jest.unmock('../server/config/requestHandler.js');
+jest.unmock('passport');
+jest.unmock('fs');
+jest.unmock('../db/db.js');
+jest.unmock('../db/config.js');
+var server = require('../server/server.js');
+
 describe('Basic server functionality', () => {
   it('Should accept and respond to GET requests at the /match endpoint', (done) => {
     $.get('http://159.203.254.178:8000/match', function(data, status) {
@@ -33,9 +43,10 @@ describe('Basic server functionality', () => {
   });
 });
 
+// Tests for database sign-in
 describe('Basic database functionality', () => {
-  it('...', (done) => {
-
+  it('Should be able to add new user to database', (done) => {
+    $.
   });
 });
 
