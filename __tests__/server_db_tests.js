855004865c4be08653610c2cf04ac9811bb6e004
Rahim <rahim.dharssi@gmail.com>
2016-04-10 13:51:46 -0700

    Write new unit test for sign-in and sign-up

--- __tests__/server_db_tests.js
+++ __tests__/server_db_tests.js
@@ -20,19 +20,19 @@ var server = require('../server/server.js');
 
 describe('Basic server functionality', () => {
   it('Should accept and respond to GET requests at the /match endpoint', (done) => {
-    $.get('http://159.203.254.178:8000/match', function(data, status) {
+    $.get('http://127.0.0.1:8000/match', function(data, status) {
       expect(status).toEqual('success');
       done();
     });
   });
   it('Should accept and respond to GET requests at the /signin endpoint', (done) => {
-    $.get('http://159.203.254.178:8000/signin', function(data, status) {
+    $.get('http://127.0.0.1:8000/signin', function(data, status) {
       expect(status).toEqual('success');
       done();
     });
   });
   it('Should not accept and respond to GET requests at the non-existant endpoints', (done) => {
-    $.get('http://159.203.254.178:8000/asdfsdf', function(data, status) {
+    $.get('http://127.0.0.1:8000/asdfsdf', function(data, status) {
       expect(status).toEqual('error');
       done();
     })
@@ -45,8 +45,30 @@ describe('Basic server functionality', () => {
 
 // Tests for database sign-in
 describe('Basic database functionality', () => {
-  it('Should be able to add new user to database', (done) => {
-    $.
+  it('Should be able to add new user to database and sign-in with that user', (done) => {
+    var newUser = { username: 'test' };
+    $.ajax({
+      method: 'POST',
+      url: '127.0.0.1:8000/signup',
+      data: newUser,
+      success: function(data) {
+        $.post('127.0.0.1:8000/signin', newUser, function(data) {
+          // TODO update the below based on your actual implementation of the sign-in request handler
+          expect(data).toEqual('going to welcome page');
+          done();
+        })
+        .fail(function() {
+          // Automatically failing if the request does not go through
+          expect(true).toEqual(false);
+          done();
+        });
+      },
+      error: function() {
+        // Automatically failing if there is an error
+        expect(true).toEqual(false);
+        done();
+      }
+    });
   });
 });
 
