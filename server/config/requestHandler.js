d5fd410e385dd5e758eb5e2c0f7bf5353f09d734
Kevin Nguyen <account@kevinwin.com>
2016-04-10 21:35:54 -0700

    Add photos to filesystem upon upload

--- server/config/requestHandler.js
+++ server/config/requestHandler.js
@@ -6,7 +6,6 @@ var request = require('request');
 // TODO update this to foursquare.js for deploy
 var foursquare = require('./foursquare.example.js');
 
-
 // The below hard-coded examples are for testing purposes. Will be removed once Foursquare API is in place.
 var restaurant = {"id":"513a4806c84c60d09153e2cc","name":"SQwers Izakaya & Sushi BAR","contact":{"phone":"4157029979","formattedPhone":"(415) 702-9979"},"location":{"address":"3015 Geary Blvd","crossStreet":"bwtn Cook St & Blake St","lat":37.781747440661505,"lng":-122.45133876800537,"distance":418,"postalCode":"94118","mayNotNeedAddress":false,"cc":"US","city":"San Francisco","state":"CA","country":"United States","formattedAddress":["3015 Geary Blvd (bwtn Cook St & Blake St)","San Francisco, CA 94118","United States"]},"categories":[{"id":"4bf58dd8d48988d1d2941735","name":"Sushi Restaurant","pluralName":"Sushi Restaurants","shortName":"Sushi","icon":{"prefix":"https://ss3.4sqi.net/img/categories_v2/food/sushi_","suffix":".png"},"primary":true}],"verified":false,"stats":{"checkinsCount":313,"usersCount":199,"tipCount":10},"url":"http://sqwers.eat24hour.com","delivery":{"id":"27042","url":"http://www.seamless.com/food-delivery/restaurant.27042.r?a=1026&utm_source=Foursquare&utm_medium=affiliate&utm_campaign=SeamlessOrderDeliveryLink","provider":{"name":"seamless"}},"allowMenuUrlEdit":true,"specials":{"count":0,"items":[]},"hereNow":{"count":0,"summary":"Nobody here","groups":[]},"referralId":"v-1460144909","venueChains":[]};
 var matchedUser = {"username":"Nathaniel","email":"nedwards@gmail.com","funfact":"I can code all the things","profileimage":"https://avatars1.githubusercontent.com/u/5132757?v=3&s=400"};
@@ -129,6 +128,7 @@ module.exports = {
   },
 
   upload: function(req, res) {
+
     var form = new formidable.IncomingForm();
     form.uploadDir = "./server/uploads";
     form.keepExtensions = true;
@@ -147,5 +147,4 @@ module.exports = {
       res.end(util.inspect({fields: fields, files: files})); // Like a console.dir
     });
   }  
-
 };
