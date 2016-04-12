d34e92bb94e293021eb2658a053941bd5f0aacef
Rahim <rahim.dharssi@gmail.com>
2016-04-12 11:01:35 -0700

    Add additional comment explaining foursquare API. Remove hard-coding of longitude and latitude.

--- server/config/foursquare.example.js
+++ server/config/foursquare.example.js
@@ -1,3 +1,8 @@
+/*
+ * The API keys below will need to be added at time of deploy in order for the app to connect with the Foursquare API.
+ * Once API keys have been added, this code will be functional.
+ */
+
 var request = require('request');
 
 exports.client_id = '';
@@ -6,13 +11,19 @@ exports.client_secret = '';
 exports.getRestaurant = function(longitude, latitude) {
   var maxResults = 50; // max # of results to return
   var searchRadius = 500; // # of meters from the specified longitude/latitude to search
-
-  /* This is the foursquare category id for 'food'. More categories can be found at https://developer.foursquare.com/categorytree. 
-   * We may want to pick a more strict category in the future as 'food' also includes some bars. If passing in multiple IDs, they
-   * should be comma separated.
-   */
   var categoryId = '4d4b7105d754a06374d81259';
 
+  /*
+   * The foursquare API provides a variety of options that can be passed in as part of the query string ('qs'):
+   *   - client_id (required)
+   *   - client_secret (required)
+   *   - v is the version of the API that you would like to access (required)
+   *   - ll is the longitude, latitude
+   *   - limit is the # of results Foursquare should return
+   *   - Category ID is the categories that you would like Foursquare to search. We have included the 'food' category below. A full list of categories 
+   *     can be found here: https://developer.foursquare.com/categorytree
+   *   - Radius is the search radius in meters. By default, Foursquare will search the entire city.
+   */
   var requestOptions = {
     method: 'GET',
     uri: 'https://api.foursquare.com/v2/venues/search',
@@ -20,7 +31,7 @@ exports.getRestaurant = function(longitude, latitude) {
       client_id: exports.client_id,
       client_secret: exports.client_secret,
       v: 20160405,
-      ll: '37.7835, -122.4089', // Hard-coding longitude, latitude for now. Should update with the longitude/latitude arguments once app can provide this data
+      ll: longitude + ', ' + latitude,
       limit: maxResults,
       categoryId: categoryId,
       radius: searchRadius
