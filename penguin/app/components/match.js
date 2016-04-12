e2b08e9bba55df893918ad2e6f27a4c137dae5c2
rahimftd <rahim.dharssi@gmail.com>
2016-04-12 12:50:44 -0700

    Revert "Add env map for development and production urls"

--- penguin/app/components/match.js
+++ penguin/app/components/match.js
@@ -3,10 +3,6 @@
 // to an icebreaker / enjoy your lunch screen.
 
 var React = require('react-native');
-var env = {
-  'development': 'http://localhost:8000/',
-  'production': 'http://159.203.254.178:8000/'
-}[process.env.NODE_ENV];
 
 var {
   Component,
@@ -60,7 +56,7 @@ class Match extends Component{
     return (
       <View style={styles.mainContainer}>
         <Image 
-        source={{uri: env + 'users/' + this.props.match.username +'/profilePhoto'}}
+        source={{uri: 'http://localhost:8000/users/' + this.props.match.username +'/profilePhoto'}}
         // resizeMode="cover"
         onLoadEnd={()=>{console.log('image actually loaded, so........')}}
         style={styles.image}
--- server/config/auth.js
+++ server/config/auth.js
@@ -1,15 +1,10 @@
 // config/auth.js
 
-var env = {
-  'development': 'http://localhost:8000/',
-  'production': 'http://159.203.254.178:8000/'
-}[process.env.NODE_ENV];
-
 module.exports = {
   facebookAuth: {
     clientID: '1626575457562968',
     clientSecret: '3d1fca39ae2d56c6e41d4fcab9f65149',
-    callbackURL: env + "auth/facebook/callback",
+    callbackURL: "http://localhost:8000/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'photos', 'email'] // get specific fields of user profile
   }
 };
