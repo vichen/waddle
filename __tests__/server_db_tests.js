93dd5a945863e39360c99f435c9493444a242f1a
Rahim <rahim.dharssi@gmail.com>
2016-04-13 15:28:53 -0700

    Fix typo in /users/:username unit test.

--- __tests__/server_db_tests.js
+++ __tests__/server_db_tests.js
@@ -176,7 +176,7 @@ describe('Username endpoint functionality', () => {
     });
   });
   it('Should not respond made to the /users/:username endpoint when the username is invalid', (done) => {
-    request.get('http://127.0.0.1:8000/users/test', (error, response, body) => {
+    request.get('http://127.0.0.1:8000/users/arglebargle', (error, response, body) => {
       if (error) {
         expect(true).toEqual(false);
         console.log('Error sending GET request to /users/:username', error);
