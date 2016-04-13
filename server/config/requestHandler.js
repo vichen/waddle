0f0a7b08b122eaecbe7b6fe9bc9d4e39603d5127
Rahim <rahim.dharssi@gmail.com>
2016-04-13 11:44:01 -0700

    Remove getHome and getSigning request handlers becuase they are not used

--- server/config/requestHandler.js
+++ server/config/requestHandler.js
@@ -66,16 +66,6 @@ var getFirstValidMatch = function(username, matchRequestsArray, userLocation) {
 
 
 module.exports = {
-  getHome: function(req, res) {
-    // TODO:
-    // if user is aleady connected with FB
-      res.send('You are home!');
-    // else
-  },
-
-  getSignin: function(req, res) {
-    res.send('get signing in...');
-  },
 
   postSignin: function(req, res) {
     // check user exist in the database
