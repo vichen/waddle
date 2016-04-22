111d2acc01626400360dc0928bd84b7ba34d1fd4
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-22 12:32:50 -0700

    add activity indicator and async storage

--- waddle/index.ios.js
+++ waddle/index.ios.js
@@ -3,47 +3,134 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
+ /**
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  */
+ 'use strict';
+
+ var React = require('react-native');
+ var {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
+  View,
+  ActivityIndicatorIOS,
+  Component,
   NavigatorIOS,
   Navigator,
-  View
-} from 'react-native';
+ } = React;
 
-// import * as Main from './app/components/main';
+ var Main = require('./app/components/main');
+ var Welcome = require('./app/components/welcome');
+ var AuthService = require('./app/components/AuthService');
+ var styles = require('./app/components/Styles');
 
-var Main = require('./app/components/main');
-var styles = require('./app/components/Styles');
-var Temp = require('./app/components/enjoy'); // used temporarily for working on styling
+ var penguin = React.createClass({
+   componentDidMount: function(){
+     AuthService.getAuthInfo((err, authInfo)=> {
+       this.setState({
+         checkingAuth: false,
+         isLoggedIn: authInfo != null
+       })
+     });
+   },
 
-class penguin extends Component {
-  render() {
+   render: function() {
+     if(this.state.checkingAuth){
+       return (
+         <View style={styles.mainContainer}>
+           <ActivityIndicatorIOS
+             animating={true}
+             size="large"
+             style={styles.loader} />
+         </View>
+       );
+     }
 
-    return (
-      <Navigator
-        initialRoute={{
-          title: 'Sign in with Waddle',
-          component: Main,
-          passProps: {}
-        }}
-        configureScene={() => {
-          return Navigator.SceneConfigs.FloatFromRight;
-        }}
-        renderScene={(route, navigator) => {
-          // count the number of func calls
-          const Component = route.component
-          return (
-            <View style={{flex: 1}}>
-              <Component navigator={navigator} route={route} {...route.passProps}/>
-            </View>
-          )
-        }}
-      />
-    );
-  }
-}
+     if(this.state.isLoggedIn){
+       return (
+         <Welcome />
+       );
+     } else{
+       return (
+         <Navigator
+           initialRoute={{
+             title: 'Sign in with Waddle',
+             component: Main,
+             passProps: {}
+           }}
+           configureScene={() => {
+             return Navigator.SceneConfigs.FloatFromRight;
+           }}
+           renderScene={(route, navigator) => {
+             // count the number of func calls
+             const Component = route.component
+             return (
+               <View style={{flex: 1}}>
+                 <Component navigator={navigator} route={route} {...route.passProps}/>
+               </View>
+             )
+           }}
+         />
+       );
+     }
+   },
+   onLogin: function(){
+     this.setState({isLoggedIn: true});
+   },
+   getInitialState: function(){
+     return {
+       isLoggedIn: false,
+       checkingAuth: true
+     }
+   }
+ });
 
-AppRegistry.registerComponent('penguin', () => penguin);
+ AppRegistry.registerComponent('penguin', () => penguin);
+
+
+// import React, {
+//   AppRegistry,
+//   Component,
+//   StyleSheet,
+//   Text,
+//   NavigatorIOS,
+//   Navigator,
+//   View
+// } from 'react-native';
+
+// // import * as Main from './app/components/main';
+
+// var Main = require('./app/components/main');
+// var styles = require('./app/components/Styles');
+// var Temp = require('./app/components/enjoy'); // used temporarily for working on styling
+
+// class penguin extends Component {
+//   render() {
+
+//     return (
+//       <Navigator
+//         initialRoute={{
+//           title: 'Sign in with Waddle',
+//           component: Main,
+//           passProps: {}
+//         }}
+//         configureScene={() => {
+//           return Navigator.SceneConfigs.FloatFromRight;
+//         }}
+//         renderScene={(route, navigator) => {
+//           // count the number of func calls
+//           const Component = route.component
+//           return (
+//             <View style={{flex: 1}}>
+//               <Component navigator={navigator} route={route} {...route.passProps}/>
+//             </View>
+//           )
+//         }}
+//       />
+//     );
+//   }
+// }
+
+// AppRegistry.registerComponent('penguin', () => penguin);
