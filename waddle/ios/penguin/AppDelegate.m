4a740c8a1c6b83f302af16f0d78c6875b28d4ae0
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-21 18:50:47 -0700

    change server config to test on phone

--- waddle/ios/penguin/AppDelegate.m
+++ waddle/ios/penguin/AppDelegate.m
@@ -32,6 +32,7 @@
    */
 
    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+   // jsCodeLocation = [NSURL URLWithString:@"http://10.6.30.206:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
