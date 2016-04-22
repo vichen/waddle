f5231fc3ed250732e267b6403bce7fc146b326ee
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-21 18:50:47 -0700

    change server config to test on phone

--- waddle/environment.js
+++ waddle/environment.js
@@ -2,8 +2,10 @@ var env = require('./env.js');
 
 var _Environments = {
   development: {
-      // IP_address: 'http://192.168.0.104:8000'
-      IP_address: 'http://10.6.31.155:8000'
+
+      IP_address: 'http://10.6.30.206:8000'
+      // IP_address: 'http://localhost:8000'
+
   }, 
   production: {
       IP_address: 'http://159.203.254.178:8000'
