778b0785c5fad54d83ff0282732ad436b631acaf
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-22 10:25:35 -0700

    modify dependency path

--- waddle/environment.js
+++ waddle/environment.js
@@ -3,8 +3,8 @@ var env = require('./env.js');
 var _Environments = {
   development: {
 
-      IP_address: 'http://10.6.30.206:8000'
-      // IP_address: 'http://localhost:8000'
+      // IP_address: 'http://10.6.30.206:8000'
+      IP_address: 'http://localhost:8000'
 
   }, 
   production: {
--- waddle/ios/penguin/AppDelegate.m
+++ waddle/ios/penguin/AppDelegate.m
@@ -31,8 +31,9 @@
    * on the same Wi-Fi network.
    */
 
-   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-   // jsCodeLocation = [NSURL URLWithString:@"http://10.6.30.206:8081/index.ios.bundle?platform=ios&dev=true"];
+   // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+   jsCodeLocation = [NSURL URLWithString:@"http://10.6.30.206:8081/index.ios.bundle?platform=ios&dev=true"];
+
 
   /**
    * OPTION 2
