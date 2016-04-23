134c22eb3b37982b23c0ea7b392755cf4a71e3df
Fred Ryder <frederickryder@gmail.com>
2016-04-23 08:53:44 -0700

    environment set-up, add Messenger.js

--- waddle/environment.js
+++ waddle/environment.js
@@ -3,7 +3,7 @@ var env = require('./env.js');
 var _Environments = {
   development: {
       // IP_address: 'http://192.168.0.104:8000'
-      IP_address: 'http://10.6.31.155:8000'
+      IP_address: 'http://localhost:8000'
   }, 
   production: {
       IP_address: 'http://159.203.254.178:8000'
