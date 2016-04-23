dddf350e6719661958afffb3c190c36aaf616f69
Roland Fung <roland.c.fung@gmail.com>
2016-04-23 13:19:43 -0700

    ignore uber

--- waddle/environment.js
+++ waddle/environment.js
@@ -1,11 +1,10 @@
-// var env = 'production';
+// this file specifies the address to the app server serving http requests
 var env = require('./env.js');
 console.log('Node environment is... ', env)
 
-// this file specifies the address to the app server serving http requests
 var _Environments = {
   development: {
-      IP_address: 'http://10.6.31.155:8000'
+      IP_address: 'http://localhost:8000'
   }, 
   production: {
       IP_address: 'https://waddlingllama.herokuapp.com'
