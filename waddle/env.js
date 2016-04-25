0f44f4bacb3f8d28c1138b03d7be5870a97c4d9a
Roland Fung <roland.c.fung@gmail.com>
2016-04-25 01:51:55 -0700

    clarify instructions

--- waddle/env.js
+++ waddle/env.js
@@ -1,5 +1,5 @@
-// set env to 'production' if you want to test against the deployed server on Heroku
-// or to 'development' if you want to test against a locally hosted server (either
-// localhost or a LAN ip address)
+// set env to 'production' if you want to test against the deployed http server on Heroku
+// or to 'development' if you want to test against a local instance of the http server (not
+// the app-dev-server, which is provided by Xcode in project's AppDelegate.m file)
 var env = 'development'; 
 module.exports = env;
--- waddle/environment.js
+++ waddle/environment.js
@@ -1,10 +1,21 @@
-// this file specifies the address to the app server serving http requests
+/* this file tells the application where to look for the http server
+ (either locally or on the deployed server). Change this setting from env.js */
 var env = require('./env.js');
 console.log('Node environment is... ', env)
 
 var _Environments = {
   development: {
-      IP_address: 'http://192.168.0.102:8000'
+      IP_address: 'http://localhost:8000'
+      /* change 'localhost' to your local IP if you want to test
+       with either an ios device or simulator on the same LAN. Leave 
+       as 'localhost' if you are just testing an xcode simulator 
+       on the same machine. 
+
+       Lastly, remember that the address of the app-dev-server 
+       is specified in Xcode's AppDelegate.m file, usually on port 8081. 
+       Use your local IP there as well if you are testing on
+       a connected ios device.  Otherwise just leave it as localhost:8081 
+       if you are only running a simulator */
   }, 
   production: {
       IP_address: 'https://waddlingllama.herokuapp.com'
